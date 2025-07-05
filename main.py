6e5fa8f33bea19a0586962a6e450c2a3fae953d1
231lukecurran@gmail.com <231lukecurran@gmail.com>
2025-07-05 14:22:25 +0100

    added links, language, limited news, added subject and control category

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
 import requests
-import smtplib
+from send_email import send_email
 
+topic = "tesla"
 api_key = "8f1ee43151d34f569522c7940c191e47"
 url = (
     "https://newsapi.org/v2/everything?"
-    "q=tesla&"
+    f"q={topic}&"
     "from=2025-06-05&"
     "sortBy=publishedAt&"
-    "apiKey=8f1ee43151d34f569522c7940c191e47"
+    "apiKey=8f1ee43151d34f569522c7940c191e47&language=en"
 )
-request = requests.get(url)
-content = request.json()
-for article in content["article"]:
-    print(article["title"])
 
+
+# Make request
+request = requests.get(url)
+
+# Get a dictionary with data
+content = request.json()
+
+# Access the article titles and description
+body = ""
+for article in content["articles"][:20]:
+    if article["title"] is not None:
+        body = "Subject: Today's news" 
+        + "\n" + body + article["title"] + "\n" \
+        + str(article["description"]) + 2*"\n" \
+        + article["url"] + 2*"\n"
+
+body = body.encode("utf-8")
+send_email(message=body)
